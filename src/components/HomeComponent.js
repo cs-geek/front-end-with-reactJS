e816ea14168f81d0a9ae15ece86289779ba8b838
Anmol Kumar Singh <anmolkumar.mca19@bmsce.ac.in>
2020-10-09 10:50:52 +0530

    Single Page Application - 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardText, CardSubtitle } from 'reactstrap';
+
+function RenderCard(props) {
+    const item = props.item;
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardText>{item.designation}</CardText> : null}
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 const Home = (props) => {
     return (
         <div className="container">
-            <h1>Home Sweet Home</h1>
+            <div className="row align-items-start">
+                <div className="col-12 col-md mt-2 mb-2">
+                    <RenderCard item={props.dish} />
+                </div>
+                <div className="col-12 col-md mt-2 mb-2">
+                    <RenderCard item={props.promo} />
+                </div>
+                <div className="col-12 col-md mt-2 mb-2">
+                    <RenderCard item={props.leader} />
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,49 +2,52 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './HomeComponent';
-import Header from './HeaderComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
+import Header from './HeaderComponent';
 import DishDetail from './DishDetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null
-    };
-  }
+		this.state = {
+			dishes: DISHES,
+			comments: COMMENTS,
+			leaders: LEADERS,
+			promotions: PROMOTIONS
+		};
+	}
 
-  // onDishSelect(dishId) {
-  //   this.setState({ selectedDish: dishId })
-  // }
+	render() {
 
-  render() {
+		const HomePage = () => {
+			return (
+				<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+					promo={this.state.promotions.filter((promo) => promo.featured)[0]}
+					leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
+			);
+		}
 
-    const HomePage = () => {
-      return (
-        <Home />
-      );
-    }
-
-    return (
-      <div>
-        <Header />
-        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
-        <Switch>
-          <Route path='/home' component={HomePage} />
-          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-          <Redirect to='/home' />
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route path='/home' component={HomePage} />
+					<Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+					<Route exact path='/contact' component={Contact} />
+					<Redirect to='/home' />
+				</Switch>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default Main;
