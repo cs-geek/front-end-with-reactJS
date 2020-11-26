70f9bb63d4d56c0c6100ed731cf61b0dc4490f56
Anmol Kumar Singh <anmolkumar.mca19@bmsce.ac.in>
2020-11-26 13:10:37 +0530

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'reactstrap';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -180,31 +181,50 @@ function RenderComments({ comments, addComment, dishId }) {
 }
 
 const DishDetail = (props) => {
-	return (
-		<div className="container">
-			<div className="row">
-				<Breadcrumb>
-					<BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-					<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-				</Breadcrumb>
-				<div className="col-12">
-					<h3>{props.dish.name}</h3>
-					<hr />
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
 				</div>
 			</div>
-			<div className="row">
-				<div className="col-12 col-md-6 col-lg-5 mt-3">
-					<RenderDish dish={props.dish} />
-				</div>
-				<div className="col-12 col-md-6 col-lg-7 mt-3">
-					<RenderComments comments={props.comments}
-						addComment={props.addComment}
-						dishId={props.dish.id} />
+		)
+	}
+	else if (props.errorMessage) {
+		return (
+			<div className="container">
+				<div className="row">
+					<h4>{props.errorMessage}</h4>
 				</div>
 			</div>
-		</div>
-
-	);
+		)
+	}
+	else if (props.dishes != null) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Breadcrumb>
+						<BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+						<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>{props.dish.name}</h3>
+						<hr />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 col-md-6 col-lg-5 mt-3">
+						<RenderDish dish={props.dish} />
+					</div>
+					<div className="col-12 col-md-6 col-lg-7 mt-3">
+						<RenderComments comments={props.comments}
+							addComment={props.addComment}
+							dishId={props.dish.id} />
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default DishDetail;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
-function RenderCard(props) {
-    const item = props.item;
-    return (
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                {item.designation ? <CardText>{item.designation}</CardText> : null}
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    );
+function RenderCard({ item, isLoading, errorMessage }) {
+    if (isLoading) {
+        return (
+            <Loading />
+        )
+    }
+    else if (errorMessage) {
+        return (
+            <h4>{errorMessage}</h4>
+        )
+    }
+    else if (item) {
+        return (
+            <Card>
+                <CardImg src={item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    {item.designation ? <CardText>{item.designation}</CardText> : null}
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        )
+    }
 }
 
 const Home = (props) => {
@@ -20,7 +32,11 @@ const Home = (props) => {
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md mt-2 mb-2">
-                    <RenderCard item={props.dish} />
+                    <RenderCard
+                        item={props.dish}
+                        isLoading={props.dishesLoading}
+                        errorMessage={props.dishesErrosMessage}
+                    />
                 </div>
                 <div className="col-12 col-md mt-2 mb-2">
                     <RenderCard item={props.promo} />
